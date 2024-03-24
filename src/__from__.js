875a2a53f7802ed0276cbadcb8b9e9aa129ddb81
Mehedi Hasan <mhedih32@gmail.com>
2024-03-25 05:58:51 +0600

    normal form added

--- src/__from__.js
+++ src/__from__.js
@@ -2,5 +2,5 @@
  * 1. use from tag and onSubmit handler with event (e)
  *  Form e access e.target.[name of the input field].value.
  * 
- * 2. use individual state.
+ * 2. controlled element:use individual field state for each input field.
 */
